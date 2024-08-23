6a2532ddd79877e02e02423fdc6d0b56591be185
eros-rama <cs.eros111@gmail.com>
2024-08-23 10:16:44 -0300

    new branch commit1

--- scripts/check_requirements_changes.sh
+++ scripts/check_requirements_changes.sh
@@ -5,6 +5,10 @@ if git diff --name-only HEAD~1 | grep -E 'requirements/prod.txt|requirements/dev
     echo "Requirements files have changed. Running compatibility checks..."
     echo 'export REQUIREMENTS_CHANGED="true"' >> $BASH_ENV
 else
+
+
+
+
     echo "Requirements files have not changed. Skipping compatibility checks..."
     echo 'export REQUIREMENTS_CHANGED="false"' >> $BASH_ENV
 
